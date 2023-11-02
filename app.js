31b11e5cac87271716f58e2cbb6804858885299b
Alena Salynskaia <al_20.07@mail.ru>
2023-11-02 13:46:54 +0300

    mongo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключение к БД
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+mongoose.connect(`${process.env.MONGO_URL}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
