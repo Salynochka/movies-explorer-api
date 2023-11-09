0e1924b519917ddc2b1c6a7f545d9e0a47cd3509
Alena Salynskaia <al_20.07@mail.ru>
2023-11-09 18:21:31 +0300

    updated jwt

--- controllers/users.js
+++ controllers/users.js
@@ -20,8 +20,6 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        secure: true,
-        sameSite: 'none',
       });
       res.send({ token }).end();
     })
