601ad664b112a059609f528403a74eb539c142c2
Alena Salynskaia <al_20.07@mail.ru>
2023-11-11 11:24:35 +0300

    /

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,8 +1,9 @@
 const limit = require('express-rate-limit');
 
 const limiter = limit({
-  windowMs: 10 * 60 * 1000, // за 10 минут
-  max: 100, // можно совершить максимум 20 запросов с одного IP
+  windowMs: 1 * 60 * 1000, // за 10 минут 10 * 60 * 1000
+  max: 100000, // можно совершить максимум 20 запросов с одного IP
+  fireImmediately: true,
 });
 
 module.exports = limiter;
